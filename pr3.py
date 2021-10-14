e316ecbc98237427fe80df81e7a8658c2d7f199a
moscvinandreea <70642112+moscvinandreea@users.noreply.github.com>
2021-10-14 18:09:08 +0300

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,8 @@
+ print("Ce nume si prenume ai?") 
+ np=str(input("Numele si prenumele este: ")) 
+ print(np) 
+ print("Ce iti doresti sa devii in viitor?") 
+ p=str(input("Profesia este: ")) 
+ print(p) 
+ print("Te numesti",np) 
+ print("Doresti sa devii",p) 
